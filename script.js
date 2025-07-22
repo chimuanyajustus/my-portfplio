ef3e9e40b014655a863b93a805394a4d17b41965
chimuanyajustus <152196892+chimuanyajustus@users.noreply.github.com>
2025-07-22 05:25:08 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("alexander site loaded!");
+// Toggle dark mode
+document.getElementById('dark-mode-toggle').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+  });
